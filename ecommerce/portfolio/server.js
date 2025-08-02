32b1e95675b668bdc13c0d73f754b75ec85b8433
Tekula Isha <ishatekula2010@gmail.com>
2025-08-02 20:19:14 +0530

    Delete ecommerce/portfolio directory

--- ecommerce/portfolio/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 8000;
-
-app.use(express.static(__dirname)); 
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
